d0c3b2293c829ba0f01c7792da413867fe04bf08
Camilo lozano <kamilo92@live.com>
2016-08-05 01:18:16 -0500

    charFinal

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,46 +1,49 @@
-/**
- * Created by Usuario on 4/08/2016.
- */
-var Chat = function (socket) {
-    this.socket = socket;
+var Chat = function(socket) {
+  this.socket = socket;
 };
 
-Chat.prototype.sendMessage = function (room, text) {
-    var message = {
-        room : room,
-        text : text
-    };
-    this.socket.emit('message', message);
+Chat.prototype.sendMessage = function(room, text) {
+  var message = {
+    room: room,
+    text: text
+  };
+
+  this.socket.emit('message', message);
 };
 
-Chat.prototype.changeRoom = function (room) {
-    this.socket.emit('join', {
-        newRoom : room
-    });
+Chat.prototype.changeRoom = function(room) {
+  this.socket.emit('join', {
+    newRoom: room
+  });
 };
 
-Chat.prototype.processCommand = function (command){
-    var words = command.split(' ');
-    var command = words[0]
-        .substring(1, words[0].length)
-        .toLowerCase();
-    var message = false;
+Chat.prototype.processCommand = function(command) {
 
-    switch (command){
-        case 'join':
-            words.shift();
-            var room = words.join(' ');
-            this.changeRoom(room);
-            break;
-        case 'nick' :
-            words.shift();
-            var nama = words.join(' ');
-            this.socket.emit('nameAttemp', name);
-            break;
-        default :
-            message = 'Unrecognized command. ';
-            break;
-    }
-    return message;
+  var words = command.split(' ')
+    // Parse command from first word
+    , command = words[0].substring(1, words[0].length).toLowerCase()
+    , message = false;
 
+  switch(command) {
+    // Handle room changing/creation
+    case 'join':
+      words.shift();
+      var room = words.join(' ');
+      this.changeRoom(room);
+      break;
+
+    // Handle name change attempts
+    case 'nick':
+      words.shift();
+      var name = words.join(' ');
+      this.socket.emit('nameAttempt', name);
+      break;
+
+    // Return an error message if the command isn't recognized
+    default:
+      message = 'Unrecognized command.';
+      break;
+  }
+
+  return message;
 };
